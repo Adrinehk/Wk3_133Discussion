97e7dc2320c84d9ef802c43b8bd397c698fc60f0
Adrinehk <147206794+Adrinehk@users.noreply.github.com>
2024-01-26 15:23:35 -0800

    added index.html, script.js and style.css file to start project

---
+++ script.js
@@ -0,0 +1,17 @@
+function myfunction ( p1, p2){
+    return p1;
+}
+
+const car = {
+    type: 'fiat',
+    model: "500",
+    color: "white"
+}
+
+
+console.log(car.color);
+
+let carName1 = "volvo";
+
+document.getElementById("1").innerHTML = "Hello";
+document.getElementById("2").innerHTML = "Bye";
